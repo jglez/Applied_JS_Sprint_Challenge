b6d9768ef4b061137f140164e3b0f89cf29d6ede
Jorge Gonzalez <jglez.cs@gmail.com>
2020-12-04 10:22:35 -0600

    Completed Header.js - Successfully appended to DOM

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,31 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+// Header component
+function Header() {
+  const headerDiv = document.createElement('div');
+  headerDiv.classList.add('header');
+
+  const spanDate = document.createElement('span');
+  spanDate.classList.add('date');
+  spanDate.textContent = 'MARCH 28, 2020';
+
+  const h1 = document.createElement('h1');
+  h1.textContent = 'Lambda Times';
+
+  const spanTemp = document.createElement('span');
+  spanTemp.classList.add('temp');
+  spanTemp.textContent = '98°';
+
+  headerDiv.appendChild(spanDate);
+  headerDiv.appendChild(h1);
+  headerDiv.appendChild(spanTemp);
+
+  return headerDiv;
+}
+// Scaffold for Header() invocation
+const scaffold = document.querySelector('.header-container');
+const newHeader = Header();
+
+// Append header to the DOM
+scaffold.appendChild(newHeader);
